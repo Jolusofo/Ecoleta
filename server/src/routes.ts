c6201eb95845607a309be02c970bdad1cfdf031b
João Lucas Souza da Fonseca <j.lucassousa76@gmail.com>
2020-06-17 23:21:27 -0300

    versin.1.2

---
+++ server/src/routes.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+
+
+import PointsController from './controllers/PointsControllers';
+import ItemsControllers from './controllers/ItemsControllers';
+
+const routes= express.Router();
+const pointsController = new PointsController();
+const itemsControllers = new ItemsControllers();
+
+ 
+
+
+
+routes.get('/items', itemsControllers.index );
+
+
+
+
+routes.post('/points',pointsController.create );
+
+routes.get('/points/', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+//index, show, create, update, delete
+
+
+export default routes;
